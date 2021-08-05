b539f038c8cf1553d35f2f649454c28554352ebf
Arnel Cellona <arnelcellona.osolink@gmail.com>
2021-08-05 14:48:21 +0800

    testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,21 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `OSOlink Solutions Web Design & Development Services`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url: `http://wordpress.oso-link.com/graphql`,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
@@ -26,7 +36,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/download.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,60 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Osolink Solution Is The Best</h1>
-    <p>Osolink Solution - Gatsby PWA To APK</p>
-    <p>This is a test message.....</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+
+const IndexPage = ({ data }) => {
+  return (
+
+
+        <Layout>
+        <Seo title="Home" />
+        <h1>Osolink Solution Is The Best</h1>
+        <p>Osolink Solution - Gatsby PWA To APK</p>
+        <p>This is a test message.....</p>
+        <h1>This is the Data that has been pulled from wordpress websites!</h1>
+        <h4> {
+        data.allWpPage.edges.map(node => (
+   
+            <li key={node.node.title}> 
+              <Link to={"http://wordpress.oso-link.com"+node.node.link}>
+              {node.node.title}  <br/>
+              </Link>
+            </li>
+         
+        ))
+      }</h4>
+        <StaticImage
+          src="../images/download.png"
+          width={300}
+          quality={95}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="A Gatsby astronaut"
+          style={{ marginBottom: `1.45rem` }}
+        />
+        <p>
+          <Link to="/page-2/">Go to page 2</Link> <br />
+          <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+        </p>
+        </Layout>
+          )
+        }
+
+export const query = graphql`
+query {
+    allWpPage {
+        edges {
+          node {
+            title
+            link
+          }
+        }
+    }
+}
+`
 
 export default IndexPage
